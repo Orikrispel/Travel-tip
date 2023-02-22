c44f87f2ca53137bf5af275a7a45eb5a6f018f5e
YohaiKorem <Yohaycasoy@gmail.com>
2023-02-22 16:28:21 +0200

    demo data adn crudl implemented

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -3,78 +3,78 @@ import { utilService } from './util.service.js'
 const PLACES_STORAGE_KEY = 'placesDB'
 
 export const locService = {
-    getLocs,
-    query,
-    get,
-    remove,
-    save
+  //   getLocs,
+  query,
+  get,
+  remove,
+  save,
 }
 
-const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
-    { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
-]
+_createPlaces()
 
-function getLocs() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(locs)
-        }, 2000)
-    })
-}
+// const locs = [
+//   { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
+//   { name: 'Neveragain', lat: 32.047201, lng: 34.832581 },
+// ]
+
+// function getLocs() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve(locs)
+//     }, 2000)
+//   })
+// }
 
 function query() {
-    return storageService.query(PLACES_STORAGE_KEY).then((places) => places)
+  return storageService.query(PLACES_STORAGE_KEY).then((places) => places)
 }
 
 function get(placeId) {
-    return storageService.get(PLACES_STORAGE_KEY, placeId)
+  return storageService.get(PLACES_STORAGE_KEY, placeId)
 }
 
 function remove(placeId) {
-    return storageService.remove(PLACES_STORAGE_KEY, placeId)
+  return storageService.remove(PLACES_STORAGE_KEY, placeId)
 }
 
 function save(place) {
-    if (place.id) {
-        return storageService.put(PLACES_STORAGE_KEY, place)
-    } else {
-        return storageService.post(PLACES_STORAGE_KEY, place)
-    }
+  if (place.id) {
+    return storageService.put(PLACES_STORAGE_KEY, place)
+  } else {
+    return storageService.post(PLACES_STORAGE_KEY, place)
+  }
 }
 
 function _createPlaces() {
-    let places = util.get(PLACES_STORAGE_KEY, places.id)
-    if (!places || !places.length) {
-        _createDemoPlaces()
-    }
+  let places = utilService.loadFromStorage(PLACES_STORAGE_KEY)
+  if (!places || !places.length) {
+    _createDemoPlaces()
+    places = utilService.loadFromStorage(places, PLACES_STORAGE_KEY)
+  }
+  return places
 }
 
 function _createDemoPlaces() {
-    const placeNames = ['Tel Aviv', 'Haifa']
-    const placePoses = [{ lat: 32.085300, lng: 34.781769 }, { lat: 32.794044, lng: 34.989571 }]
+  const placeNames = ['Tel Aviv', 'Haifa']
+  const placePoses = [
+    { lat: 32.0853, lng: 34.781769 },
+    { lat: 32.794044, lng: 34.989571 },
+  ]
 
-    const places = placeNames.map((placeName, i) => {
-        const place = _createPlace(placeName, placePoses[i])
-        return place
-    })
-    utilService.saveToStorage(PLACES_STORAGE_KEY, places)
+  const places = placeNames.map((placeName, i) => {
+    const place = _createPlace(placeName, placePoses[i])
+    return place
+  })
+  utilService.saveToStorage(PLACES_STORAGE_KEY, places)
 }
-
 
 function _createPlace(name, pos) {
-    return {
-        id: utilService.makeId(),
-        name,
-        pos: {
-            lat,
-            lng
-        },
-        weather: '',
-        createdAt: Date.now(),
-        updatedAt: ''
-    }
+  return {
+    id: utilService.makeId(),
+    name,
+    pos,
+    weather: '',
+    createdAt: Date.now(),
+    updatedAt: '',
+  }
 }
-
-
-
