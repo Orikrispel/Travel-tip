204cdf7d726b44be1d8dff881fcdd420e2cdc94d
YohaiKorem <Yohaycasoy@gmail.com>
2023-02-22 20:59:13 +0200

    more work on rendering by query string params

--- js/app.controller.js
+++ js/app.controller.js
@@ -11,98 +11,104 @@ window.renderPlacesList = renderPlacesList
 window.onSearch = onSearch
 
 function onInit() {
-    mapService
-        .initMap()
-        .then(() => {
-            console.log('Map is ready')
-        })
-        .catch(() => console.log('Error: cannot init map'))
-    renderPlacesList()
+  mapService
+    .initMap()
+    .then(() => {
+      console.log('Map is ready')
+    })
+    .catch(() => console.log('Error: cannot init map'))
+  renderPosByQueryStringParams()
+  renderPlacesList()
 }
 
 function renderPosByQueryStringParams() {
-    const queryStringParams = new URLSearchParams(window.location.search)
-    const pos = queryStringParams.get('pos')
-    if (!pos) return
+  const queryStringParams = new URLSearchParams(window.location.search)
+  const lat = +queryStringParams.get('lat')
+  const lng = +queryStringParams.get('lng')
+  Promise.resolve(mapService.panTo(lat, lng))
+  if (!lat || !lng) return
 }
 
 // This function provides a Promise API to the callback-based-api of getCurrentPosition
 function getPosition() {
-    // console.log('Getting Pos')
-    return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject)
-    })
+  // console.log('Getting Pos')
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject)
+  })
 }
 
 function onAddMarker() {
-    console.log('Adding a marker')
-    mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 })
+  console.log('Adding a marker')
+  mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 })
 }
 
 // From placeKeeper:
 function onDeleteLoc(elBtn) {
-    const placeId = elBtn.dataset.id
-    locService.remove(placeId).then(() => renderPlacesList())
+  const placeId = elBtn.dataset.id
+  locService.remove(placeId).then(() => renderPlacesList())
 
-    //   locService.query().then((locs) => {
-    //     console.log('Locations:', locs)
-    // document.querySelector('.locs').innerText = JSON.stringify(locs, null, 2)
-    //   })
+  //   locService.query().then((locs) => {
+  //     console.log('Locations:', locs)
+  // document.querySelector('.locs').innerText = JSON.stringify(locs, null, 2)
+  //   })
 }
 
 // From placeKeeper:
 function renderPlacesList() {
-    locService.query().then((locs) => {
-        const list = document.querySelector('.places-list')
-        let strHTMLs = []
-        strHTMLs = locs.map(
-            (loc) => `<div class="place-container">${loc.name}
+  locService.query().then((locs) => {
+    const list = document.querySelector('.places-list')
+    let strHTMLs = []
+    strHTMLs = locs.map(
+      (loc) => `<div class="place-container">${loc.name}
         <button class="btn-pan" data-id="${loc.id}" onclick="onPanTo(this)">Go</button>
         <div class="btn-delete" data-id="${loc.id}" onclick="onDeleteLoc(this)">✗</div>
       </div>`
-        )
-        list.innerHTML = strHTMLs.join('')
-    })
+    )
+    list.innerHTML = strHTMLs.join('')
+  })
 }
 
 function onGetLocs() {
-    locService.query().then((locs) => {
-        // console.log('Locations:', locs)
-        renderPlacesList(locs)
-        // document.querySelector('.locs').innerText = JSON.stringify(locs, null, 2)
-    })
+  locService.query().then((locs) => {
+    // console.log('Locations:', locs)
+    renderPlacesList(locs)
+    // document.querySelector('.locs').innerText = JSON.stringify(locs, null, 2)
+  })
 }
 
 function onGetUserPos() {
-    getPosition()
-        .then((pos) => {
-            // console.log('User position is:', pos.coords)
-            document.querySelector(
-                '.user-pos'
-            ).innerText = `Latitude: ${pos.coords.latitude} - Longitude: ${pos.coords.longitude}`
-        })
-        .catch((err) => {
-            console.log('err!!!', err)
-        })
+  getPosition()
+    .then((pos) => {
+      // console.log('User position is:', pos.coords)
+      document.querySelector(
+        '.user-pos'
+      ).innerText = `Latitude: ${pos.coords.latitude} - Longitude: ${pos.coords.longitude}`
+    })
+    .catch((err) => {
+      console.log('err!!!', err)
+    })
 }
 
 function onPanTo(elBtn) {
-    const placeId = elBtn.dataset.id
-    locService
-        .get(placeId)
-        .then((place) => mapService.panTo(place.pos.lat, place.pos.lng))
+  const placeId = elBtn.dataset.id
+  locService
+    .get(placeId)
+    .then((place) => mapService.panTo(place.pos.lat, place.pos.lng))
 }
 
 function onSearch(ev, term) {
-    ev.preventDefault()
-    mapService.getPlacePos(term)
-        .then(pos => {
-            mapService.addMarker(pos)
-            mapService.panTo(pos.lat, pos.lng)
-            locService.addPlace(term, pos)
-                .then(() => renderPlacesList())
-
-        })
-
-
+  ev.preventDefault()
+  mapService.getPlacePos(term).then((pos) => {
+    mapService.addMarker(pos)
+    mapService.panTo(pos.lat, pos.lng)
+    locService.addPlace(term, pos).then(() => renderPlacesList())
+    const queryStringParams = `?lat=${pos.lat}&lng=${pos.lng}`
+    const newUrl =
+      window.location.protocol +
+      '//' +
+      window.location.host +
+      window.location.pathname +
+      queryStringParams
+    window.history.pushState({ path: newUrl }, '', newUrl)
+  })
 }
